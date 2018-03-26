b001c4af06f1075612186d7ca9c5db52c9d635c4
fastforwad <36497870+fastforwad@users.noreply.github.com>
2018-03-27 00:46:06 +0800

    Initial commit

---
+++ jquerystuff.js
@@ -0,0 +1,18 @@
+
+ 	$(document).ready(function() {
+
+		$(".cross").hide();
+		$("nav ul").hide();
+		$(".hamburger").click(function() {
+			$(".hamburger").fadeOut("slow");
+			$(".cross").fadeIn();
+			$("nav ul").slideToggle("slow");
+		});
+
+		$(".cross").click(function() {
+			$(".cross").fadeOut();
+			$("nav ul").slideToggle("slow", function() {
+			$(".hamburger").fadeIn();
+			});
+		});
+	});
