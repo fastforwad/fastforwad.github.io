7107d830492124b58f5f8b18d157567a4c1e5cb4
fastforwad <36497870+fastforwad@users.noreply.github.com>
2018-04-22 15:59:28 +0800

    lightgallery
    lightgallery lololololol

--- jquerystuff.js
+++ jquerystuff.js
@@ -1,18 +1,23 @@
+$(document).ready(function () {
+	$("#lightgallery").lightGallery({
+		loadYoutubeThumbnail: true,
+		youtubeThumbSize: 'default',
+		loadVimeoThumbnail: true,
+		vimeoThumbSize: 'thumbnail_medium',
+	});
+	console.log($("#lightgallery").lightGallery);
+	$(".cross").hide();
+	$("nav ul").hide();
+	$(".hamburger").click(function () {
+		$(".hamburger").fadeOut("slow");
+		$(".cross").fadeIn();
+		$("nav ul").slideToggle("slow");
+	});
 
- 	$(document).ready(function() {
-
-		$(".cross").hide();
-		$("nav ul").hide();
-		$(".hamburger").click(function() {
-			$(".hamburger").fadeOut("slow");
-			$(".cross").fadeIn();
-			$("nav ul").slideToggle("slow");
-        });
-
-		$(".cross").click(function() {
-			$(".cross").fadeOut();
-			$("nav ul").slideToggle("slow", function() {
+	$(".cross").click(function () {
+		$(".cross").fadeOut();
+		$("nav ul").slideToggle("slow", function () {
 			$(".hamburger").fadeIn();
-			});
 		});
 	});
+});
